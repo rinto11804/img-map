c8058bebb40386504c31e7486980fd2ca77b6dfb
Rinto Joseph TR <70879873+rinto11804@users.noreply.github.com>
2023-04-10 12:42:35 +0000

    framework

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use base64::{engine::general_purpose, Engine as _};
+use image::ImageOutputFormat;
+use std::io::Cursor;
+use wasm_bindgen::prelude::*;
+use web_sys::{window, HtmlImageElement};
+
+#[wasm_bindgen]
+extern "C" {
+    // #[wasm_bindgen(js_namespace = console)]
+    // fn log(s: &str);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[wasm_bindgen]
+pub fn grayscale() -> Result<(), wasm_bindgen::JsValue> {
+    let doc = window().unwrap().document().unwrap();
+    let img = doc
+        .get_element_by_id("img")
+        .unwrap()
+        .dyn_into::<HtmlImageElement>()?;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let image = image::load_from_memory(&img.src().as_bytes()).unwrap();
+    // let grayscale_img = image.grayscale();
+
+    let mut image_data: Vec<u8> = Vec::new();
+    image
+        .write_to(&mut Cursor::new(&mut image_data), ImageOutputFormat::Png)
+        .unwrap();
+    let res_base64 = general_purpose::STANDARD.encode(&image_data);
+    let src = format!("data:image/png;base64,{}", res_base64);
+   img.set_src(&src);
+
+    let body = doc.body().unwrap();
+    body.append_child(&img)?;
+    Ok(())
 }
